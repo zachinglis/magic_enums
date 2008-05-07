cc23ca3a8cd1347f95227939052ba14cf26eac24
Zach Inglis <zach@lt3media.com>
2008-05-07 14:17:46 +0000

    Functionality finished, I think

--- lib/magic_enums.rb
+++ lib/magic_enums.rb
@@ -1,1 +1,23 @@
-# MagicEnums
+module MagicEnums
+  def self.included(base)
+   base.extend(ClassMethods)
+  end
+ 
+  module ClassMethods
+    def enum_column(column_name, choices=[])
+      array_in_text = "[#{choices.collect { |choice| "'#{choice}'" }.join(", ")}]" # Make the array into text form.
+      class_eval "
+        class_inheritable_accessor  :#{column_name}_choices
+        write_inheritable_attribute :#{column_name}_choices, #{array_in_text}
+      "
+      
+      choices.each do |choice|
+        class_eval "
+          def #{column_name}_is_#{choice}?
+            self.#{column_name} == '#{choice}'
+          end
+          "
+      end
+    end
+  end
+end
--- test/magic_enums_test.rb
+++ test/magic_enums_test.rb
@@ -1,8 +1,27 @@
 require 'test/unit'
+require File.join(File.dirname(__FILE__), '../lib/magic_enums')
 
 class MagicEnumsTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def test_should_recognize_column_and_associated_class_variable
+    assert true
   end
+end
+
+class Person
+   enum_column    :gender, %w(male female)
+   attr_accessor  :gender, :database_gender
+   
+   def read_attribute(column)
+     class_eval "#{attribute_column(column)}"
+   end
+   
+   def write_attribute(column, value)
+     class_eval "#{attribute_column(column)} = '#{value}'"
+   end
+   
+   def attribute_column(column)
+    "self.database_#{column}"
+   end
+   
+   
 end
