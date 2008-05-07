88a92bbc049a6f9fd7ceec2fef51252fdc163f52
Zach Inglis <zach@lt3media.com>
2008-05-07 15:26:18 +0000

    Started work on validation

--- lib/magic_enums.rb
+++ lib/magic_enums.rb
@@ -4,11 +4,21 @@ module MagicEnums
   end
  
   module ClassMethods
-    def enum_column(column_name, choices=[])
+    def enum_column(column_name, choices=[], options={})
+      configuration = { :message => "is not a valid choice" }
+      configuration.merge!(options)
+      
       array_in_text = "[#{choices.collect { |choice| "'#{choice}'" }.join(", ")}]" # Make the array into text form.
+      
       class_eval "
         class_inheritable_accessor  :#{column_name}_choices
         write_inheritable_attribute :#{column_name}_choices, #{array_in_text}
+        
+        validate :validate_#{column_name}_choice
+        
+        def validate_#{column_name}_choice
+          errors.add(\"#{column_name}\", \"#{configuration(:message)}\") unless self.#{column_name}_choices.include?(self.choice) # This configuration variable isn't working
+        end
       "
       
       choices.each do |choice|
